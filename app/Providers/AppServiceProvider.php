bf9fe93028f3b4cf8b91cc8f031682331838c9ea
grvaz <shirokov.val@gmail.com>
2023-09-30 18:59:17 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\DbLogger;
+use App\Services\Logger;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->bind(Logger::class, DbLogger::class);
     }
 }
